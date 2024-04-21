d2d2679dce38bc313a6afd8b06847578a38ac481
Joban Mendpara <jobanmendpara@gmail.com>
2024-04-21 09:04:33 -0400

    feat: all pages match system theme

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const isDark = useDark();
+</script>
 
 <template>
-  <body class="container mx-auto">
+  <body
+    class="container mx-auto"
+    :class="isDark ? 'dark' : ''"
+  >
     <slot />
   </body>
 </template>
--- layouts/main.vue
+++ layouts/main.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-import { useDark } from '@vueuse/core';
 import queries from '~/queries';
 
 const { $user } = useNuxtApp();
-
-const isDarkMode = useDark();
 
 const { data: user, status } = useQuery({
   ...queries.app.user($user.value!.id),
@@ -16,7 +13,7 @@ const { data: user, status } = useQuery({
 <template>
   <body
     v-if="status === 'success' && user"
-    :class="`${isDarkMode ? 'dark' : ''} h-screen overflow-hidden mx-auto grid grid-rows-[auto,1fr] grid-cols-1 md:grid-rows-1 md:grid-cols-[auto,1fr]`"
+    class="h-screen overflow-hidden mx-auto grid grid-rows-[auto,1fr] grid-cols-1 md:grid-rows-1 md:grid-cols-[auto,1fr]"
   >
     <NavBar
       :role="user.role"
